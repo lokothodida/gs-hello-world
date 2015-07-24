5ff40a1378f08163791f0f9e92c52f9dbf18a30a
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-24 13:44:05 +0100

    Update register.php
    Add more documentation

--- hello_world/register.php
+++ hello_world/register.php
@@ -1,26 +1,51 @@
 <?php
 
-// Include the plugin wrapper class
+// Now inside of the scope of the anonymous function, we can register the
+// the plugin, and we are completely safe with declaring new variables (so
+// our actions here will not interfere with other plugins).
+
+// The /lib/ directory has a wrapper class called GSPlugin, which can safely
+// be @included and gives us a nice collection of methods for more easily
+// registering our plugin's details and actons.
 include 'lib/gsplugin.php';
 
-// Instantiate the GSPlugin class and register static plugin information
+// Instantiate the GSPlugin class and register the static plugin information.
+// The $id variable is given to by the parameter passed in the anonymous
+// function, so it doesn't need to hard-code statically
+// This means that you can easily rename your plugin without breaking it
+// simply by renaming the hello_world.php file and hello_world/ folder
+// accordingly
 $plugin = new GSPlugin(array(
-  'id'          => $id,
-  'version'     => '1.0',
-  'author'      => 'You',
-  'website'     => 'http://yourwebsite.com',
-  'tab'         => 'theme',
-  'lang'        => 'en_US',
+  'id'       => $id,
+  'version'  => '1.0',
+  'author'   => 'You',
+  'website'  => 'http://yourwebsite.com',
+  'tab'      => 'theme',
+  'lang'     => 'en_US',
 ));
 
-// Add theme footer hook (shows "Hello World!" message in footer)
-$plugin->hook('footer', 'frontend/show.php');
+// Because the 'tab' field is 'theme' in the above registration, the admin
+// panel of this plugin will appear in the 'Theme' section
+// However, we won't be able to access it unless there is a link to it in
+// the sidebar! So let's add a sidebar:
+$plugin->hook('sidebar', 'createSideMenu', array($plugin->id(), $plugin->i18n('PLUGIN_SIDEBAR')));
 
-// Add a sidebar
-$plugin->hook('sidebar','createSideMenu', array($plugin->id(), $plugin->i18n('PLUGIN_SIDEBAR')));
+// The above code registers a hook to create the sidebar using the plugin's
+// ID, and with a label that is language-sensitive (the hash for the label is
+// defined in the /lang/ directory)
 
-// Point admin panel content to admin/index.php script
+// Now for the admin panel itself
+// This call will execute the index.php script (in the /backend/ directory
+// of your plugin) when an admin accesses your plugin's admin panel.
+// In that script, we define what should happen and what should be shown on
+// the admin panel.
 $plugin->admin('backend/index.php');
 
-// Initialize
+// Now let us add a hook to display something on the front end.
+// The below hook will be executed in the theme's footer when the front end
+// user loads the page.
+// It will run the show.php script (in this plugin's /frontend/ directory)
+$plugin->hook('footer', 'frontend/show.php');
+
+// Finally, initialize the plugin:
 $plugin->init();
