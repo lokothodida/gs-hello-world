67891b4870bb33d670407bfb600e93e600aee1bd
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-24 13:48:12 +0100

    Update index.php
    Add more documentation

--- hello_world/backend/index.php
+++ hello_world/backend/index.php
@@ -1,1 +1,11 @@
-<p>I like to echo "Hello World" in the footers of all themes.</p>
+<?php
+
+// This script is executed when an admin accesses your plugin from the admin
+// panel. As this is a very simple example, all it will do is display a message
+// on the page (albeit sensitive to the language of the installation).
+// The GSPlugin class executes this script and imports a variable named @plugin
+// which allows you to access all of the methods that you would have from your
+// GSPlugin instance. This allows us to use the @i18n method like so:
+
+?>
+<p><?php echo $plugin->i18n('HELLO_WORLD_ADMIN'); ?></p>
