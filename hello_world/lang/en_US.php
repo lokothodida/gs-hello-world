07aa9a5f3c5f943f4ff55eb679d821f951643507
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-25 12:44:21 +0100

    Add language files

--- hello_world/lang/en_US.php
+++ hello_world/lang/en_US.php
@@ -4,7 +4,7 @@
 // English Translation of Hello World example plugin
 $i18n = array();
 $i18n['PLUGIN_TITLE']       = 'Hello World';
-$i18n['PLUGIN_DESCRIPTION'] = 'Finds email addresses in content and components and "hides" them';
+$i18n['PLUGIN_DESCRIPTION'] = 'Outputs "Hello World!" in footers of all themes.';
 $i18n['PLUGIN_SIDEBAR']     = 'Hello World description';
 $i18n['HELLO_WORLD_MSG']    = 'Hello World!';
 $i18n['HELLO_WORLD_ADMIN']  = 'I like to echo "Hello World!" in the footers of all themes.';
---
+++ hello_world/lang/ru_RU.php
@@ -0,0 +1,10 @@
+<?php
+
+// ru_RU.php
+// Russian Translation of Hello World example plugin
+$i18n = array();
+$i18n['PLUGIN_TITLE']       = 'Привет Мир';
+$i18n['PLUGIN_DESCRIPTION'] = 'Выходы " Привет Мир ! " В нижней части всех темах.';
+$i18n['PLUGIN_SIDEBAR']     = 'Привет мир описание';
+$i18n['HELLO_WORLD_MSG']    = 'Привет Мир!';
+$i18n['HELLO_WORLD_ADMIN']  = 'Я бы повторить " Привет Мир ! " В нижние колонтитулы всех темах.';
