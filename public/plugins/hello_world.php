07b9eed81c9cc34c1723117905ede80833efd84e
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-30 09:45:52 +0100

    Change to php 5.3+ closure

--- public/plugins/hello_world.php
+++ public/plugins/hello_world.php
@@ -1,17 +1,10 @@
 <?php
 
-// This file @includes the /register.php script in your plugin's folder
+// This file @includes the /index.php script in your plugin's folder
 // but protects the scope of the code in that file by wrapping the execution
-// in an anonymous, immediately invoked function(*).
+// in an anonymous, immediately invoked function.
 // It also uses the name of this file as the @id for the rest of the plugin
-// and imports that variable into the scope of the /register.php script.
-call_user_func(create_function('$id', '
-  include($id . "/register.php");
-'), basename(__FILE__, ".php"));
-
-// (*) @create_function is being used instead of a Closure because GetSimple
-//     supports PHP 5.2+, and Closures were only introduced in 5.3. Ideally
-//     the code would look like this (PHP 5.3+):
-//     call_user_func(function($id) {
-//       include $id . '/register.php';
-//     }, basename(__FILE__, '.php'));
+// and imports that variable into the scope of the /index.php script.
+call_user_func(function($id) {
+  include $id . '/index.php';
+}, basename(__FILE__, '.php'));
