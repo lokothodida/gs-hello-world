0b0385fa0994eec7550872fc372b71efaa14d5d1
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-08-04 17:51:24 +0100

    Create functions file (uses closure)

---
+++ public/plugins/example_settings/backend/settings_functions.php
@@ -0,0 +1,55 @@
+<?php
+
+// Closure to protect variables related to these common functions
+return function($params = array()) {
+
+// Import the i18n helper method
+$i18n = $params['i18n'];
+
+// File we will save the data to
+$file = $params['datapath']. '/mysettings.json';
+
+// Default data for the file
+$default = array(
+  'setting1' => 'value1',
+);
+
+// Methods
+$settings = array();
+
+// Get current settings
+$settings['get'] = function() use ($file) {
+  $contents = file_get_contents($file);
+  return (array) json_decode($contents);
+};
+
+// Save settings
+$settings['save'] = function($data) use ($file) {
+  $contents = json_encode($data);
+  return file_put_contents($file, $contents);
+};
+
+// Initialize
+$settings['init'] = function() use ($file, $settings, $default) {
+  if (!file_exists($file)) {
+    $settings['save']($default);
+  }
+};
+
+// Display a form
+$settings['displayform'] = function($data) use ($i18n) {
+  include 'form.php';
+};
+
+// Display error message
+$settings['displaystatus'] = function($status, $msg) use ($i18n) {
+  $class = $status ? 'updated' : 'error';
+  $css = json_encode($class);
+  $msg = json_encode($msg);
+  include 'status.php';
+};
+
+// Expose the methods
+return $settings;
+
+};
