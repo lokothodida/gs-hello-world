f370241baaf582fd5cdc3f84d248801807053fe2
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-08-04 17:51:41 +0100

    Change index to use new functions

--- public/plugins/example_settings/backend/index.php
+++ public/plugins/example_settings/backend/index.php
@@ -1,18 +1,33 @@
 <?php
 
-include 'functions.php';
+// Load the settings functions
+$export = require 'settings_functions.php';
 
-$settings = new \ExampleSettings\Data(GSDATAOTHERPATH);
-$settings->init();
+// Import variables into the closure
+$methods = $export(array(
+  'i18n' => $i18n,
+  'datapath' => GSDATAOTHERPATH,
+));
 
+// Pull the methods from the array into the scope of this included file
+extract($methods);
+
+// Initialize the settings
+$init();
+
+// Save any changes
 if (!empty($_POST)) {
-  include 'savedata.php';
+  try {
+    $save($_POST);
+    $displaystatus(true, $i18n('SETTINGS_SAVE_SUCC'));
+  } catch (Exception $error) {
+    $displaystatus(false, $i18n('SETTINGS_SAVE_ERR'));
+  }
 }
 
+// Display the form
 try {
-  $data = $settings->get();
-  include 'viewsettings.php';
+  $displayform($get());
 } catch (Exception $error) {
-  echo $error->getMessage();
-  echo 'error';
+  $displaystatus(false, $i18n('SETTINGS_SHOW_ERR'));
 }
