01ee95514020adb5ba8a3869d820c010cf6ece8a
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-08-05 10:52:22 +0100

    Make admin method anonymous and add more comments

--- public/plugins/example_settings/index.php
+++ public/plugins/example_settings/index.php
@@ -1,17 +1,17 @@
 <?php
 
+// Load the language files
 i18n_merge($id) || i18n_merge($id, 'en_US');
 
+// Create i18n wrapper
 $i18n = function ($hash) use ($id) {
   return i18n_r($id . '/' . $hash);
 };
 
-$actions = array();
+// Since this plugin isn't doing anything on the front end, we'll just
+// define the admin panel anonymously
 
-$actions['admin'] = function() use ($i18n) {
-  include 'backend/index.php';
-};
-
+// Register the plugin
 call_user_func_array('register_plugin', array(
   'pluginid'    => $id,
   'title'       => $i18n('PLUGIN_TITLE'),
@@ -20,9 +20,13 @@ call_user_func_array('register_plugin', array(
   'website'     => 'http://yoursite.com',
   'description' => $i18n('PLUGIN_DESCRIPTION'),
   'admintab'    => 'settings',
-  'adminpanel'  => $actions['admin']
+  'adminpanel'  => function() use ($i18n) {
+    // All admin panel logic will be contained in the backend/index.php file
+    include 'backend/index.php';
+  }
 ));
 
+// Add the sidebar
 add_action('settings-sidebar', 'createSideMenu', array(
   'pluginid' => $id,
   'label'    => $i18n('PLUGIN_SIDEBAR')
