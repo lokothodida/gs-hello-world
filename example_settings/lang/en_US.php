f54a3b375634bdccb060e358d558640414d846b7
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2015-08-09 13:11:56 +0000

    Add more i18n hashes

--- example_settings/lang/en_US.php
+++ example_settings/lang/en_US.php
@@ -6,3 +6,9 @@ $i18n = array();
 $i18n['PLUGIN_TITLE']       = 'Example Settings';
 $i18n['PLUGIN_DESCRIPTION'] = 'Provides example of a settings page';
 $i18n['PLUGIN_SIDEBAR']     = 'Example Settings';
+$i18n['SETTING_2']          = 'Setting 2';
+$i18n['SETTING_3']          = 'Setting 3';
+$i18n['SETTINGS_GET_ERR']   = 'Error getting settings';
+$i18n['SETTINGS_SAVE_SUCC'] = 'Settings saved';
+$i18n['SETTINGS_SAVE_ERR']  = 'Error saving settings';
+
