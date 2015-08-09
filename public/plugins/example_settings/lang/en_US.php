12e753b81851071eacaf950a065961d7798653b0
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2015-08-09 10:39:17 +0000

    Moved all files to root of repository

--- public/plugins/example_settings/lang/en_US.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// en_US.php
-// English Translation of Example Settings plugin
-$i18n = array();
-$i18n['PLUGIN_TITLE']       = 'Example Settings';
-$i18n['PLUGIN_DESCRIPTION'] = 'Provides example of a settings page';
-$i18n['PLUGIN_SIDEBAR']     = 'Example Settings';
